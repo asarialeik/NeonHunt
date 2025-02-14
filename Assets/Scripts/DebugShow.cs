8090007992db9dfee3f1803ab701acf0de5e2a3d
asarialeik <asarialeik@gmail.com>
2025-02-14 08:57:24 +0000

    V7

---
+++ Assets/Scripts/DebugShow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DebugShow : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI fpsText;
+    float fpsNumber;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
